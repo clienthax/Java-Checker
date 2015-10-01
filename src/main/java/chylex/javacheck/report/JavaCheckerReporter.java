12655c6e434e5fa4c784d0b4eec5f2edc97d8cb1
BuildTools <clienthax@gmail.com>
2015-10-01 16:32:54 +0100

    make links clickable

--- src/main/java/chylex/javacheck/report/JavaCheckerReporter.java
+++ src/main/java/chylex/javacheck/report/JavaCheckerReporter.java
@@ -1,10 +1,14 @@
 package chylex.javacheck.report;
-import java.util.Map;
-import javax.swing.JOptionPane;
 import net.minecraftforge.fml.relauncher.FMLRelaunchLog;
 import net.minecraftforge.fml.relauncher.IFMLCallHook;
 import org.apache.commons.lang3.JavaVersion;
 import org.apache.commons.lang3.SystemUtils;
+import org.lwjgl.Sys;
+
+import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import java.util.Map;
 
 public final class JavaCheckerReporter implements IFMLCallHook{
 	public static JavaVersion minVersion = null;
@@ -13,8 +17,21 @@ public final class JavaCheckerReporter implements IFMLCallHook{
 	public Void call() throws Exception{
 		if (!SystemUtils.isJavaVersionAtLeast(minVersion)){
 			FMLRelaunchLog.severe(getConsoleReport());
-			JOptionPane.showMessageDialog(null, "<html>"+getWindowReport()+"</html>","Outdated Java",JOptionPane.ERROR_MESSAGE);
-            		throw new OutdatedJavaException();
+
+			JEditorPane ep = new JEditorPane("text/html", "<html>"+getWindowReport()+"</html>");
+			ep.addHyperlinkListener(new HyperlinkListener()
+			{
+				@Override
+				public void hyperlinkUpdate(HyperlinkEvent e)
+				{
+					if (e.getEventType().equals(HyperlinkEvent.EventType.ACTIVATED))
+						Sys.openURL(e.getURL().toString());
+				}
+			});
+			ep.setEditable(false);
+			JOptionPane.showMessageDialog(null, ep, "Outdated Java", JOptionPane.ERROR_MESSAGE);
+
+			throw new OutdatedJavaException();
 		}
 		
 		return null;
@@ -24,7 +41,7 @@ public final class JavaCheckerReporter implements IFMLCallHook{
 	public void injectData(Map<String,Object> data){}
 	
 	static String getConsoleReport(){
-		return new StringBuilder(242).append("\n")
+		return new StringBuilder().append("\n")
 		.append("\n!! DO NOT REPORT !!\n\n")
 		.append("Pixelmon requires Java "+minVersion.toString()+" or newer, you are using ").append(SystemUtils.JAVA_VERSION).append(".\n")
 		.append("If you are running Windows you can\n")
@@ -38,14 +55,14 @@ public final class JavaCheckerReporter implements IFMLCallHook{
 	}
 	
 	static String getWindowReport(){
-		return new StringBuilder(230)
+		return new StringBuilder()
 		.append("Pixelmon requires Java "+minVersion.toString()+" or newer, you are using ").append(SystemUtils.JAVA_VERSION).append(".<br>")
 		.append("If you are running Windows you can<br>")
-		.append("visit <span style=\"color:red\">https://java.com/download/</span> for the latest version.<br>")
+		.append("visit <span style=\"color:red\"><a href=\"https://java.com/download\">https://java.com/download/</a></span> for the latest version.<br>")
 		.append("Please, uninstall the old version first to prevent further issues.")
 		.append("<br>")
 		.append("If you are running OSX Lion (10.7) or newer, Please use the following launcher<br>")
-		.append("<span style=\"color:red\">http://launcher.mojang.com/download/Minecraft-staging.dmg</span>")
+		.append("<span style=\"color:red\"><a href=\"http://launcher.mojang.com/download/Minecraft-staging.dmg\">http://launcher.mojang.com/download/Minecraft-staging.dmg</a></span>")
 		.toString();
 	}
 }
